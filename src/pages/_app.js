6bde50c05aeee90509bbc388c5ca56cee1dec56c
Sefa <95241108+unuvarx@users.noreply.github.com>
2024-03-01 17:20:51 +0300

    first-commit

---
+++ src/pages/_app.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "../styles/global.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap"
+        rel="stylesheet"
+      />
+      <Component {...pageProps} />{" "}
+    </React.Fragment>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function index() {
+    return ( <div >
+
+
+        <div>
+            home
+        </div>
+    </div>
+    )
+}
