fef12b839b0b98e44774f29f54c54be86b0acfab
Sefa <95241108+unuvarx@users.noreply.github.com>
2024-03-04 11:47:27 +0300

    updated homepage

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,71 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "@/components/slider/slider.module.scss";
 
 export default function Slider() {
-    
   return (
     <div className={styles.sliderContainer}>
-      <div className={styles.slider}>
-        <div className={styles.slideTrack}>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png"
-              height={100}
-              width={250}
-              alt=""
-            />
+      <div className={styles.container}>
+        <div className={styles.row}>
+          <div className={`${styles.container} ${styles.rounded}`}>
+            <div className={styles.slider}>
+              <div className={styles.logos}>
+                <img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg" alt="" />
+              </div>
+              <div className={styles.logos}>
+                <img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg" alt="" />
+                <img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg" alt="" />
+              </div>
+            </div>
           </div>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png"
-              height={100}
-              width={250}
-              alt=""
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png"
-              height={100}
-              width={250}
-              alt=""
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/4.png"
-              height={100}
-              width={250}
-              alt=""
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png"
-              height={100}
-              width={250}
-              alt=""
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png"
-              height={100}
-              width={250}
-              alt=""
-            />
-          </div>
-          <div className={styles.slide}>
-            <img
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png"
-              height={100}
-              width={250}
-              alt=""
-            />
-          </div>
-          
         </div>
       </div>
     </div>
   );
 }
+
+
+
+
